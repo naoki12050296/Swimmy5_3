8bf87e2e3755866ccc4fe61b8675675204bb96e0
naoki12050296 <n_h.noo_bow.125@icloud.com>
2017-08-20 13:48:37 +0900

    nice commit

--- src/swimmypractice5/Main.java
+++ src/swimmypractice5/Main.java
@@ -3,63 +3,53 @@ package swimmypractice5;
 public class Main {
 
 	public static void main(String[] args) throws Exception {
+		try {
+			//携帯ショップのインスタンスを生成
+			MobileShop shop = new MobileShop();
 
+			//Iphoneの機種を選択し、名前を設定
+			Iphone iphone = shop.choiceIphone();
 
-	try {
+			//CPUをセットするメソッドを呼び出し
+			iphone.inputAndSetCpu();
 
-		//携帯ショップのインスタンスを生成
-		MobileShop shop = new MobileShop();
+			//RAMをセット
+			iphone.inputAndSetRam();
 
-		//Iphoneの機種を選択し、名前を設定
-		Iphone iphone = shop.choiceIphone();
+			//ROMをセット
+			iphone.inputAndSetRom();
 
-		//CPUをセットするメソッドを呼び出し
-		iphone.inputAndSetCpu();
+			//選ばれた機種に応じたメールを送信する（ポリモーフィズム）
+			iphone.sendMail();
 
-		//RAMをセット
-		iphone.inputAndSetRam();
+			//音楽DLメソッドをオーバーライド
+			iphone.music();
 
-		//ROMをセット
-		iphone.inputAndSetRom();
+			//LINE電話メソッドを呼び出し
+			iphone.tell(6255);
 
-		//選ばれた機種に応じたメールを送信する（ポリモーフィズム）
-		iphone.sendMail();
+			//androidの機種を選択し、名前を設定
+			Android android = shop.choiceAndroid();
 
-		//音楽DLメソッドをオーバーライド
-		iphone.music();
+			//CPU情報をセット
+			android.inputAndSetCpu();
 
-		//LINE電話メソッドを呼び出し
-		iphone.tell(6255);
+			//RAMをセット
+			android.inputAndSetRam();
 
-		//androidの機種を選択し、名前を設定
-		Android android = shop.choiceAndroid();
+			//ROMをセット
+			android.inputAndSetRom();
 
-		//CPU情報をセット
-		android.inputAndSetCpu();
+			//選ばれた機種に応じたメールを送信する（ポリモーフィズム）
+			android.sendMail();
 
-		//RAMをセット
-		android.inputAndSetRam();
+			//音楽DLメソッドをオーバーライド（ROMをセットしていないので例外発生）
+			android.music();
 
-		//ROMをセット
-		android.inputAndSetRom();
-		//phone.inputAndSetRom();
-
-		//選ばれた機種に応じたメールを送信する（ポリモーフィズム）
-		android.sendMail();
-
-		//音楽DLメソッドをオーバーライド（ROMをセットしていないので例外発生）
-		android.music();
-
-		//音声通話メソッドを呼び出し
-		android.tell(80, "090-1234-5678");
-
+			//音声通話メソッドを呼び出し
+			android.tell(80, "090-1234-5678");
+		} catch(CheckException e) {
+			e.printStackTrace();
+		}
 	}
-
-	catch (CheckException e) {
-		e.printStackTrace();
-	}
-
-	}
-
-
 }
